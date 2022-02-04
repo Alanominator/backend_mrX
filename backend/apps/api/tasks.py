a48b8152105ee12a0dc439e6c087d9530c62da41
alanominator <alanominator@protonmail.com>
2022-02-04 20:10:33 +0500

    added salary

---
+++ backend/apps/api/tasks.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, unicode_literals
+
+from celery import shared_task
+
+@shared_task
+def add(x, y):
+    return x + y
+
+
+
+# add.delay(4,4)
+
+# add.apply_async((3,3), countdown=10)
